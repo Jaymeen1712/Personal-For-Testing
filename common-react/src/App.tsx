d6d79aaf702cd5710a48105c330ee8635ae4617f
Jaymeen Sonara <jaymeen.sonara@rapidops.com>
2023-08-23 10:42:27 +0530

    Revert "Created react app"
    This reverts commit 5c2d216d509a6cfde7c11cd5d211099a058a6b2b.

--- common-react/src/App.tsx
+++ common-react/src/App.tsx
@@ -2,19 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import randomColor from "./utils/random-color";
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard/dashboard";
-import New3dGraph from "./components/graph/3d-graph";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" Component={Dashboard} />
-        <Route path="/3d" Component={New3dGraph}/>
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
